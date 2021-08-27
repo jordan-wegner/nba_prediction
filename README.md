35e702f881985806b7d3404cf2647da22c6428ea
Jordan <jordan.a.wegner@gmail.com>
2021-08-27 18:26:51 -0500

    successful test run of report

--- README.md
+++ README.md
@@ -12,11 +12,18 @@
 ## How to Run  
 
 main.R  
-`Rscript -e "rmarkdown::render('main.R', params = list(year=2021, skipscrape='Yes'),output_file='test_run.html',clean=TRUE)"`
+`Rscript -e "rmarkdown::render('main.R', params = list(year=2021, skipscrape='Yes',rookies='Yes', model_name='pcr_model.rds', num_comp=47),output_file='test_run.html',clean=TRUE)"`
 
 ## To Do 
 
 Migrate report generation and dashboard over   
 Check normalization vs no normalization   
 Add all stars to teams  
-Check training and cross validation methods  
+Source function the modeling portion  
+
+## 2021-2022 Modeling Results 
+Best Model: PCR  
+Rookies vs. No Rookies: No Rookies  
+Number of Comparisons: 47  
+
+
--- main.R
+++ main.R
@@ -12,6 +12,12 @@
 #'    value: x
 #'  skipscrape:
 #'    value: x
+#'  rookies:
+#'    value: x
+#'  model_name:
+#'    value: x
+#'  num_comp:
+#'    value: x
 #' 
 #' ---
 
@@ -125,9 +131,113 @@ loginfo("writing modeling data sets to .csv")
 write.csv(nba2NR, paste0("modeling_no_rookies_",seasonID,".csv"), row.names = F)
 write.csv(nba2R, paste0("modeling_with_rookies_",seasonID,".csv"), row.names = F)
 
-#+ text, echo=F
-#'
-#'Just a test of text 
+rookies <- as.character(params$rookies)
+model_name <- as.character(params$model_name)
+num_comp <- params$num_comp
+
+#' # Prediction Information 
+#' **Year**: `r year`  
+#' **Data includes rookies?**: `r rookies`  
+#' **Model Used**: `r model_name`  
+#' **Number of Comparisons**: `r num_comp`  
+#' 
+
+#+ rooks, echo = F
+
+if (rookies=="Yes"){
+  nba <- nba2R
+} else if (rookies=="No"){
+  nba <- nba2NR
+} else {
+  print("ERROR: PLEASE SPECIFY rookies ARG")
+}
+#+ reading model, echo = F
+suppressMessages(library(dplyr))
+suppressMessages(library(stringr))
+
+full_year <- nba %>% filter(season == upcomingSeason) 
+set_year <- full_year %>% select(-c(won,season,tm))
+
+model <- readRDS(model_name)
+
+#+ predicting, echo = F
+
+suppressMessages(library(pls))
+
+model_predictions <- as.data.frame(predict(model, set_year, ncomp = num_comp))
+
+colnames(model_predictions) <- c('predicted')
+model_predictions$team <- full_year$tm
+model_predictions$ab <- str_sub(full_year$tm, 1, nchar(full_year$tm) - 5)
+
+#+ gettingConf, echo = F
+
+conf <- readxl::read_excel('team_conf.xlsx')
+
+pred_year <- merge(model_predictions, conf, by = 'ab')
+
+suppressMessages(library(sqldf))
+
+predictions_year <- sqldf('SELECT * FROM pred_year
+      ORDER BY conference, predicted DESC')
+colnames(predictions_year) <- c('ab','pred','team_year','team','conf','div')
+
+#+ writing, echo = F
+
+write.csv(predictions_year, paste0("/Users/jordanwegner/Desktop/nba2/predictions/", Sys.Date(), '-predictions.csv'))
+
+ec_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE conf = 'East' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+atl_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Atlantic' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+se_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Southeast' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+ce_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Central' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+
+wc_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE conf = 'West' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+nw_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Northwest' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+pac_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Pacific' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+sw_champ <- sqldf("SELECT team FROM predictions_year 
+                  WHERE div = 'Southwest' 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+nba_champ <- sqldf("SELECT team FROM predictions_year 
+                  ORDER BY pred DESC 
+                  LIMIT 1")
+
+#' ## Eastern Conference 
+#' **Eastern Conference Champion**: `r ec_champ`   
+#' **Atlantic Division Champion**: `r atl_champ`   
+#' **Southeast Division Champion**: `r se_champ`   
+#' **Central Division Champion**: `r ce_champ`   
+#'   
+#' ## Western Conference 
+#' **Western Conference Champion**: `r wc_champ`   
+#' **Northwest Division Champion**: `r nw_champ`   
+#' **Southwest Division Champion**: `r sw_champ`   
+#' **Pacific Division Champion**: `r pac_champ`   
+#' 
+#' ## NBA CHAMPION:
+#' `r nba_champ`
+
 
 
 
