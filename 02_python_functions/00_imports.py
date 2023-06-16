fa0f6b7626f4a57298800c4d030324fba84e7e60
Jordan <jordan.a.wegner@gmail.com>
2023-06-16 16:18:01 -0500

    player data set done

---
+++ 02_python_functions/00_imports.py
@@ -0,0 +1,7 @@
+# all required imports 
+
+# basics 
+import pandas as pd
+import numpy as np
+
+# get_new_players 
---
+++ 02_python_functions/nba_playoff_prediction.py
@@ -0,0 +1,6 @@
+import get_new_players
+
+# Main Script
+print("BEGIN: get_new_players.py")
+get_new_players.get_new_players()
+print("END: get_new_players.py")
