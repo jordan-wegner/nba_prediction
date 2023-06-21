f96070d96fa0a77d6960ce7ad7d575a6adce8735
Jordan <jordan.a.wegner@gmail.com>
2023-06-21 09:04:25 -0500

    changes

--- 02_python_functions/nba_playoff_prediction.py
+++ 02_python_functions/nba_playoff_prediction.py
@@ -1,6 +1,12 @@
 import get_new_players
+import get_new_teams
 
 # Main Script
+
 print("BEGIN: get_new_players.py")
 get_new_players.get_new_players()
 print("END: get_new_players.py")
+
+print("BEGIN: get_new_teams.py")
+get_new_teams.get_new_teams()
+print("END: get_new_teams.py")
