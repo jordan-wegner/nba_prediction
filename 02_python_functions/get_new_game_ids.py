28a94a6aaf23f5e9b85d073cea947ac59f581c48
jordan-wegner <67288554+jordan-wegner@users.noreply.github.com>
2023-06-27 14:32:13 -0500

    added league_id_nullable='00'

--- 02_python_functions/get_new_game_ids.py
+++ 02_python_functions/get_new_game_ids.py
@@ -14,7 +14,7 @@ def get_new_game_ids():
     if any(old_gids['SEASON_YEAR'].str.contains(current_gids)):
         print ("No new Game IDs to add. No update required.")
     else:
-        new_gids = teamgamelogs.TeamGameLogs(season_nullable=current_gids).get_data_frames()[0][['SEASON_YEAR','TEAM_ID','GAME_ID']]
+        new_gids = teamgamelogs.TeamGameLogs(season_nullable=current_gids,league_id_nullable='00').get_data_frames()[0][['SEASON_YEAR','TEAM_ID','GAME_ID']]
         old_file_id = "/users/jordanwegner/Desktop/nba2/03_data/00_archive/game_ids_"+ (datetime.datetime.today() - timedelta(1)).strftime('%Y%m%d')+".csv" 
         old_gids.to_csv(old_file_id, index=False)
         new_gids['GAME_ID'] = 'G'+new_gids['GAME_ID']
