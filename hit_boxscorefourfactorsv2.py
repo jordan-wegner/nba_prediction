9f890b74b8de6938fd977248961298a1aff81719
jordan-wegner <67288554+jordan-wegner@users.noreply.github.com>
2023-06-27 14:02:03 -0500

    Create hit_boxscorefourfactorsv2.py

---
+++ hit_boxscorefourfactorsv2.py
@@ -0,0 +1,10 @@
+def hit_boxscorefourfactorsv2(gs):
+    # takes the game id data as input 
+    # returns the four factors data for each player in each game
+    bsffs = []
+    for i in gs['GAME_ID']:
+        bsff = boxscorefourfactorsv2.BoxScoreFourFactorsV2(game_id=i[1:]).get_data_frames()[0]
+        bsffs.append(bsff)
+    BSFF = pd.concat(bsffs)
+    BSFF['GAME_ID'] = 'G'+BSFF['GAME_ID']
+    return BSFF    
