6514708bdd683ba126024fbdaa1fcc0602bd8522
jordan-wegner <67288554+jordan-wegner@users.noreply.github.com>
2023-06-27 10:54:05 -0500

    add G to game ids

--- 02_python_functions/get_new_game_ids.py
+++ 02_python_functions/get_new_game_ids.py
@@ -17,6 +17,7 @@ def get_new_game_ids():
         new_gids = teamgamelogs.TeamGameLogs(season_nullable=current_gids).get_data_frames()[0][['SEASON_YEAR','TEAM_ID','GAME_ID']]
         old_file_id = "/users/jordanwegner/Desktop/nba2/03_data/00_archive/game_ids_"+ (datetime.datetime.today() - timedelta(1)).strftime('%Y%m%d')+".csv" 
         old_gids.to_csv(old_file_id, index=False)
+        new_gids['GAME_ID'] = 'G'+new_gids['GAME_ID']
         updated_gids = pd.concat([old_gids,new_gids])
         updated_gids.to_csv('/users/jordanwegner/Desktop/nba2/03_data/game_ids.csv',index=False) 
 
