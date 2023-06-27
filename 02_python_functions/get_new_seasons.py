1a5d4097e8f098cf55c09e1bc0f0769c1ffd5146
jordan-wegner <67288554+jordan-wegner@users.noreply.github.com>
2023-06-27 14:06:13 -0500

    Update get_new_seasons.py
    updated comments

--- 02_python_functions/get_new_seasons.py
+++ 02_python_functions/get_new_seasons.py
@@ -1,6 +1,3 @@
-# need season id
-# probably a function to get the upcoming season id and add it to an existing file
- 
 import pandas as pd
 import numpy as np
 import datetime
@@ -8,7 +5,7 @@ from datetime import timedelta
  
 def get_new_seasons():
     # no input
-    old_seasons = pd.read_csv("/users/jordanwegner/Desktop/nba2/03_data/seasons.csv") # update this path
+    old_seasons = pd.read_csv("/users/jordanwegner/Desktop/nba2/03_data/seasons.csv")
     y1 = datetime.datetime.today().strftime('%Y')
     Y2 = (datetime.datetime.today() - datetime.timedelta(days = -365)).strftime('%Y')
     y2 = (datetime.datetime.today() - datetime.timedelta(days = -365)).strftime('%y')
@@ -22,6 +19,6 @@ def get_new_seasons():
         if (int(y1)-int(updated_seasons['y1'][len(updated_seasons)-2]))!=1:
             raise TypeError("Error in Seasons!")
         else:
-            old_file_id = "/users/jordanwegner/Desktop/nba2/03_data/00_archive/seasons_"+ (datetime.datetime.today() - timedelta(1)).strftime('%Y%m%d')+".csv" # updated this path
+            old_file_id = "/users/jordanwegner/Desktop/nba2/03_data/00_archive/seasons_"+ (datetime.datetime.today() - timedelta(1)).strftime('%Y%m%d')+".csv" 
             old_seasons.to_csv(old_file_id, index=False)
-            updated_seasons.to_csv('/users/jordanwegner/Desktop/nba2/03_data/seasons.csv',index=False) # update this path
+            updated_seasons.to_csv('/users/jordanwegner/Desktop/nba2/03_data/seasons.csv',index=False) 
