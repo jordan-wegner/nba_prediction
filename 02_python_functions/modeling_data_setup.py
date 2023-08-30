a72f46b028e0e5fdc9b7d944abd6ab692542eab0
Jordan <jordan.a.wegner@gmail.com>
2023-08-30 16:21:10 -0500

    updated doc for pred output

--- 02_python_functions/modeling_data_setup.py
+++ 02_python_functions/modeling_data_setup.py
@@ -1,6 +1,10 @@
 def modeling_data_setup(data,test_seasons,this_season,model_type):
     # returns x, xtest, y, ytest, this season's data 
     
+    target = 'PO_WINS'
+    
+    data = data.copy()
+    
     import pandas as pd
     import numpy as np 
     
@@ -20,16 +24,8 @@ def modeling_data_setup(data,test_seasons,this_season,model_type):
     from sklearn.model_selection import train_test_split
     
     print('columns to drop:')
-    drop_cols = ['TEAM_SEASON','TEAM_ID','TEAM_NAME','PO_WINS','PO_WINS_CAT','SEASON']
+    drop_cols = ['TEAM_SEASON','TEAM_ID','TEAM_NAME','PO_WINS','SEASON']
     print(drop_cols)
-    
-    print('Model Type: {}'.format(model_type))
-    if model_type == 'cat':
-        target = 'PO_WINS_CAT'
-    elif model_type == 'reg':
-        target = 'PO_WINS'
-    else:
-        raise TypeError("ERROR! Enter model_type! Options: 'cat' or 'reg'")
     
     print('Creating SEASON for splitting')
     ids = data['TEAM_SEASON'].str.split('_')
@@ -39,6 +35,9 @@ def modeling_data_setup(data,test_seasons,this_season,model_type):
     print("Dropping this season: {}".format(this_season))
     current = data[data['SEASON']==this_season].copy()
     
+    print('Dropping the Pelicans and Hornets first season')
+    data = data[~data['LS_WIN_PCT'].isna()].copy()
+    
     test = data[data['SEASON'].isin(test_seasons)].copy()
     train = data[(~data['SEASON'].isin(test_seasons))&(data['SEASON']!=this_season)].copy()
     ytest = test[target].copy()
@@ -46,6 +45,6 @@ def modeling_data_setup(data,test_seasons,this_season,model_type):
     xtest = test.drop(drop_cols,axis=1)
     xtrain = train.drop(drop_cols,axis=1)
     
-    print("Returning: X Train, X Test, Y Train, Y Test, and the Upcoming Season")
+    print("Returning: Train, Test, X Train, X Test, Y Train, Y Test, and the Upcoming Season")
     
-    return xtrain,xtest,ytrain,ytest,current
+    return train,test,xtrain,xtest,ytrain,ytest,current
